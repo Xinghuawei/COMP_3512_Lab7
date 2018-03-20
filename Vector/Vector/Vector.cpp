f84ca8dfa62c2ca0f3fd3cec7085bc89bd6b1891
Xinghuawei <wxh52794@gmail.com>
2018-03-19 21:27:00 -0700

    sum

---
+++ Vector/Vector/Vector.cpp
@@ -0,0 +1,46 @@
+#include "Vector.h"
+
+int Vector::sum(const std::vector<int>& v) {
+	int sum{ 0 };
+	for (int i = 0; i < v.size(); i++) {
+		sum += v[i];
+	}
+
+	return sum;
+}
+
+int Vector::max(const std::vector<int>& v) {
+	int largest = v[0];
+
+	for (int i = 1; i < v.size(); i++) {
+		if (v[i] > largest) {
+			largest = v[i];
+		}
+	}
+
+	return largest;
+}
+
+int Vector::min(const std::vector<int>& v) {
+	int small = v[0];
+
+	for (int i = 1; i < v.size(); i++) {
+		if (v[i] < small) {
+			small = v[i];
+		}
+	}
+
+	return small;
+}
+
+double Vector::average(const std::vector<int>& v) {
+	double avg{ 0.0 };
+	double sum{ 0.0 };
+
+	for (int i = 0; i < v.size(); i++) {
+		sum += v[i];
+	}
+	avg = sum / (double)v.size();
+	return avg;
+
+}
---
+++ Vector/Vector/Vector.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+class Vector {
+public:
+	int sum(const std::vector<int>&);
+
+	int max(const std::vector<int>&);
+
+	int min(const std::vector<int>&);
+
+	double average(const std::vector<int>&);
+};
